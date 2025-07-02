3380acbad58f7e1e5731c1d2ba812cf75fe48c06
Lucas <lgs.08lucas@gmail.com>
2025-07-02 13:40:40 -0300

    Iniciado Projeto.

---
+++ src/main/java/br/com/alura/screensound/ScreenSoundApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.screensound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenSoundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenSoundApplication.class, args);
+    }
+
+}
